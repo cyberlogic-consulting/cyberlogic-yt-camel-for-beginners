73120d31fef327c47b3bb18dbac8e43c21cba469
ikulaga <kulagaivanandreevich@gmail.com>
2024-12-30 17:06:14 +0500

    apache camel with SB video

---
+++ camel-sb-example/Readme.md
@@ -0,0 +1,2 @@
+# Apache Camel with Spring Boot for Beginners
+Code samples demonstrated in https://youtu.be/y2h5sUYDl34 .
---
+++ camel-sb-example/src/main/java/camel/examples/camelsbexample/CamelSbExampleApplication.java
@@ -0,0 +1,13 @@
+package camel.examples.camelsbexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamelSbExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CamelSbExampleApplication.class, args);
+    }
+
+}
