1890fb88e77c94075be9afefc99ea27c25ab246e
Babak Vahdat <bvahdat@apache.org>
2025-01-21 23:29:17 +0100

    make use of the latest Camel LTS version (excluding the quarkus example)

--- camel-sb-example/src/test/java/ch/cyberlogic/camel/examples/route/ExampleRouteTest.java
+++ camel-sb-example/src/test/java/ch/cyberlogic/camel/examples/route/ExampleRouteTest.java
@@ -12,7 +12,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
@@ -25,7 +25,7 @@ public class ExampleRouteTest {
     @Autowired
     private CamelContext camelContext;
 
-    @MockitoBean
+    @MockBean
     private ExampleMessageTransformer transformerMock;
 
     private final String directTestEndpoint = "direct:example-route-test";
